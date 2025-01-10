14993eb0859b154a86da59d14c9eedcf13e573a9
jordansalagala21 <officjordansal@gmail.com>
2025-01-10 06:58:17 -0500

    Updated scripts for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jazzy-garage/', // Set the base path to your repository name
+  build: {
+    outDir: 'dist', // Ensure the output directory matches Vercel's expectations
+  },
 });
