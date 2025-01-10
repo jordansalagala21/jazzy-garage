419f188bdb377de2eabf17492cd45d107432036f
jordansalagala21 <officjordansal@gmail.com>
2025-01-10 07:01:14 -0500

    Updated scripts for Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -6,5 +7,6 @@ import App from "./App";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>
 );
